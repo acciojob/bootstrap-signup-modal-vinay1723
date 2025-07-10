010aea2b2c1caf0e4cc199dfee4271c0c63528fe
vinay1723 <144577819+vinay1723@users.noreply.github.com>
2025-07-10 15:44:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 //your JS code here. If required.
-const myModal = document.getElementById("myModal");
-const myInput = document.getElementById("myInput");
+// const myModal = document.getElementById("myModal");
+// const myInput = document.getElementById("myInput");
 
-myModal.addEventListener("shown.bs.modal", () => {
-	e.preventDefault();
-  myInput.focus();
-});
+// myModal.addEventListener("shown.bs.modal", () => {
+// 	e.preventDefault();
+//   myInput.focus();
+// });
